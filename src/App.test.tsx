7e5d4ff300a048fac3fb9ebe3de8ccee24db948b
Olof Sandell <contact@olofsandell.com>
2021-06-09 11:37:07 +0200

    chore(testing): correct the test description

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
-test('renders learn react link', () => {
+test('renders a label containing "Saturation"', () => {
   render(<App />)
   const linkElement = screen.getAllByText(/Saturation/i)
   expect(linkElement[0]).toBeInTheDocument()
