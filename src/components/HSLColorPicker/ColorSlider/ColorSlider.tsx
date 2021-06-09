4ec6251a1abaf473ff5a31b18807b6183be0cf20
Olof Sandell <contact@olofsandell.com>
2021-06-09 09:33:52 +0200

    style: change from double to single quotes

--- src/components/HSLColorPicker/ColorSlider/ColorSlider.tsx
+++ src/components/HSLColorPicker/ColorSlider/ColorSlider.tsx
@@ -1,8 +1,8 @@
-import { ReactElement, FC, ChangeEvent } from "react"
-import { makeStyles } from "@material-ui/core/styles"
-import Slider from "@material-ui/core/Slider"
-import Box from "@material-ui/core/Box"
-import Typography from "@material-ui/core/Typography"
+import { ReactElement, FC, ChangeEvent } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Slider from '@material-ui/core/Slider'
+import Box from '@material-ui/core/Box'
+import Typography from '@material-ui/core/Typography'
 
 // interfaces
 interface ColorSliderProps {
@@ -17,50 +17,50 @@ interface ColorSliderProps {
 
 const useStyles = makeStyles<null, ColorSliderProps>({
   root: {
-    height: "1em",
-    padding: "0px",
-    margin: "10px 0",
-    "& .MuiSlider-thumb.Mui-focusVisible": {
-      boxShadow: "2px 2px 5px 0px rgba(0,0,0,0.75)",
+    height: '1em',
+    padding: '0px',
+    margin: '10px 0',
+    '& .MuiSlider-thumb.Mui-focusVisible': {
+      boxShadow: '2px 2px 5px 0px rgba(0,0,0,0.75)',
     },
-    "& .MuiSlider-thumb.MuiSlider-active": {
-      width: "1.8em",
-      height: "1.8em",
-      marginLeft: "-0.9em",
-      borderRadius: "0.9em",
-      top: "-0.4em",
-      boxShadow: "2px 2px 5px 0px rgba(0,0,0,0.75)",
+    '& .MuiSlider-thumb.MuiSlider-active': {
+      width: '1.8em',
+      height: '1.8em',
+      marginLeft: '-0.9em',
+      borderRadius: '0.9em',
+      top: '-0.4em',
+      boxShadow: '2px 2px 5px 0px rgba(0,0,0,0.75)',
     },
   },
   rail: {
     background: (props) => props.background,
-    borderRadius: "0.5em",
+    borderRadius: '0.5em',
 
-    height: "100%",
-    top: "0",
-    opacity: "1",
+    height: '100%',
+    top: '0',
+    opacity: '1',
   },
   track: {
-    background: "transparent",
-    height: "10px",
+    background: 'transparent',
+    height: '10px',
   },
   thumb: {
     color: (props) => props.thumbColor,
-    borderRadius: "0.8em",
-    top: "-0.3em",
-    width: "1.6em",
-    height: "1.6em",
-    marginLeft: "-0.8em",
-    marginTop: "0",
-    border: "0.4em solid #faf6f6",
-    boxShadow: "2px 2px 5px 0px rgba(0,0,0,0.75)",
-    "&:hover": {
-      width: "1.8em",
-      height: "1.8em",
-      marginLeft: "-0.9em",
-      borderRadius: "0.9em",
-      top: "-0.4em",
-      boxShadow: "2px 2px 5px 0px rgba(0,0,0,0.75)",
+    borderRadius: '0.8em',
+    top: '-0.3em',
+    width: '1.6em',
+    height: '1.6em',
+    marginLeft: '-0.8em',
+    marginTop: '0',
+    border: '0.4em solid #faf6f6',
+    boxShadow: '2px 2px 5px 0px rgba(0,0,0,0.75)',
+    '&:hover': {
+      width: '1.8em',
+      height: '1.8em',
+      marginLeft: '-0.9em',
+      borderRadius: '0.9em',
+      top: '-0.4em',
+      boxShadow: '2px 2px 5px 0px rgba(0,0,0,0.75)',
     },
   },
 })
@@ -77,7 +77,7 @@ const ColorSlider: FC<ColorSliderProps> = (props): ReactElement => {
 
   return (
     <Box display="flex" alignItems="center">
-      <Typography display="block" variant="caption" style={{ width: "100px" }}>
+      <Typography display="block" variant="caption" style={{ width: '100px' }}>
         {props.label}
       </Typography>
       <Slider
